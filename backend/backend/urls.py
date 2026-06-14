b747a5d300b4e28dda3f035c49bce8c5406db563
Uzair-Waseem-390 <uzairwaseem390@gmail.com>
2026-06-15 00:29:18 +0500

    adding root_url

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -2,12 +2,22 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
+from rest_framework.decorators import api_view, permission_classes
 
+# here define the rool url function
 
-
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def root_url(request):
+    return Response({
+        'message': 'Welcome to UpSkiller API'
+    })
 
 
 urlpatterns = [
+    path('', root_url),
     path('admin/', admin.site.urls),
     path('api/users/', include('users.urls')),
     path('api/roadmaps/', include('roadmaps.urls')),
